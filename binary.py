3458ef51de15b1972049c8d043558795f3f278a1
Todd Weisse <taweisse@gmail.com>
2021-03-24 01:27:56 -0400

    blip generation working with easing, may want to increase easing period somehow

--- binary.py
+++ binary.py
@@ -8,10 +8,13 @@ import math
 import struct
 from bitstring import BitArray
 
-SAMP_RATE = 44100
-FREQ = 5000
+SAMP_RATE  = 44100
+FREQ       = 4000
+SAMP_DEPTH = 2
 
 SEP_CYCLES = 3
+SHT_CYCLES = 3
+LNG_CYCLES = 6
 
 # Generates samples for a number of cycles of a sin wave at a given frequency.
 def generateSin(freq, cycles = 1):
@@ -25,16 +28,43 @@ def generateSin(freq, cycles = 1):
 		curAng += angStep
 	return vals
 
+def generateBlip(targetFreq, length):
+	# Round up to the nearest whole number of samples.
+	totalSamples = math.ceil((SAMP_RATE / targetFreq) * length)
+
+	# Offset the start of the wave to -1. We use cosine for simplicity here,
+	# otherwise we have to deal with pi / 4.
+	startAngle = math.pi
+	endAngle = (2 * length + 2) * math.pi
+	angleInc = (endAngle - startAngle) / totalSamples
+
+	# Generate samples.
+	sampleScaler = 32767
+	blipVals = []
+	curAngle = startAngle
+	for step in range(totalSamples):
+		val = int(sampleScaler * math.cos(curAngle))
+
+		# Ease the start & end of the wave.
+		if curAngle - startAngle <= math.pi:
+			val = int((val + sampleScaler) / 2)
+		elif endAngle - curAngle <= math.pi:
+			val = int((val - sampleScaler) / 2)
+		
+		blipVals.append(val)
+		curAngle += angleInc
+	return blipVals	
+
 # Generates samples for amplitude-encoding a byte as audio.
 def encodeByteAmplitude(byte):
 	samples = []
 	bitStr = BitArray(byte).bin
 	for bit in bitStr:
 		if bit == '0':
-			samples.extend(generateSin(FREQ, 2))
+			samples.extend(generateBlip(FREQ, 2))
 		else:
-			samples.extend(generateSin(FREQ, 5))
-		samples.extend([0] * (SAMP_RATE / FREQ) * SEP_CYCLES)
+			samples.extend(generateBlip(FREQ, 5))
+		samples.extend([0] * math.ceil((SAMP_RATE / FREQ) * SEP_CYCLES))
 	return samples
 
 if __name__ == "__main__":
@@ -61,7 +91,7 @@ if __name__ == "__main__":
 	# Open & configure the output WAV file.
 	with wave.open(outFilename, 'wb') as outFile:
 		outFile.setnchannels(1) # Mono.
-		outFile.setsampwidth(2) # 16-bit.
+		outFile.setsampwidth(SAMP_DEPTH) # 16-bit.
 		outFile.setframerate(SAMP_RATE)
 		
 		# Open the input file and read bytes.
@@ -70,14 +100,7 @@ if __name__ == "__main__":
 			while byte != b'':
 				# Encode each byte of the input file as audio.
 				data = encodeByteAmplitude(byte)
-				outFile.writeframes(struct.pack('%si' % len(data), *data))
+				outFile.writeframes(struct.pack('%sh' % len(data), *data))
 				
 				# Read the next byte.
 				byte = inFile.read(1)
-		
-		# for i in range(10000):
-		# 	data = generateSin(FREQ, 5)
-		# 	f.writeframes(struct.pack('%si' % len(data), *data))
-		# 	data = generateSin(FREQ / 2, 2)
-		# 	f.writeframes(struct.pack('%si' % len(data), *data))
-
